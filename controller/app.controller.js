c9748920d6e357d443dc5b02ae3b817dcd148236
hanbiro <vanchauha0125@gmail.com>
2024-05-22 16:47:05 +0700

    add: add new feat for img

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,5 +1,21 @@
 angular.module('BookStoreApp', []).controller("BookStoreController", function ($scope) {
     $scope.searchValue = "";
-    
+    $scope.selectedImageIndex = null;
+    $scope.images = [
+        './assets/images/main-banner-1.jpg',
+        './assets/images/main-banner-2.jpg',
+        './assets/images/main-banner-3.jpg',
+        './assets/images/main-banner-4.jpg',
+        './assets/images/main-banner-5.jpg',
+        './assets/images/main-banner-6.jpg',
+    ];
+
+    $scope.selectImage = function(index) {
+        $scope.selectedImageIndex = index;
+    };
+
+    $scope.isSelected = function(index) {
+        return $scope.selectedImageIndex === index;
+    };
 });
  
